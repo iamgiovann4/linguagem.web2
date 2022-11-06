f2a7a73b83fe4f07131c0b9397e2ad6cd0cb82e1
iamgiovann4 <giovannasiqueiranicolau@gmail.com>
2022-11-05 22:31:35 -0300

    calculos

--- projeto/scripts.js
+++ projeto/scripts.js
@@ -27,8 +27,6 @@ function cadastrarIngredientes() {
     var iqtdC = document.getElementById('qtdC');
     var ivalor = document.getElementById('valor');
 
-    // var iqtd = parseFloat(iqtdA) + parseFloat(iqtdC); //soma a quantidade em kg/g dos ingredientes.
-
     cadastroI.ingt = iIngrediente.value;
     cadastroI.qtdA = iqtdA.value;
     cadastroI.qtdC = iqtdC.value;
@@ -57,25 +55,30 @@ function x() {
     var corpoTabela1 = document.getElementById("corpoTabela1");
     corpoTabela1.innerHTML=""; //O corpo tabela 1 irá receber um texto.
 
-    for (var i = 0; i < novoP.length; i++) {  
-        for (var j = 0; j < novoI.length; j++) {
-            var trPessoa = document.createElement("tr"); //tr é linha.
-            var td1 = document.createElement("td"); //td é célula dentro da linha.
+    var trPessoa = document.createElement("tr"); //tr é linha.
+    var tdNome = document.createElement("td"); //td é célula dentro da linha.
+    tdValorTotal = document.createElement("td");
 
-            var cadastroP = novoP[i];
+    for (var index = 0; index < novoP.length; index++) {
+        
+        var cadastroP = novoP[index];
 
-            td1.innerText=cadastroP.nome;
-            trPessoa.appendChild(td1);
+        tdNome.innerText=cadastroP.nome;
+        trPessoa.appendChild(tdNome);
 
-            corpoTabela1.appendChild(trPessoa);
-            td6 = document.createElement("td");
+        tdValorTotal.innerText="R$ 00,00";
+        trPessoa.appendChild(tdValorTotal);
+
+        corpoTabela1.appendChild(trPessoa);
+
+        for (var index = 0; index < novoI.length; index++) {
             
-            var cadastroI = novoI[j];
-            
-            // qtdTotal = parseFloat(cadastroI.qtdA) + parseFloat(2 * (cadastroI.iqtdC * cadastroI.valor));
-            
-            td6.innerText = "Oiiii";
-            trPessoa.appendChild(td6);
+            var cadastroI = novoI[index];
+
+            var total = (parseFloat(cadastroI.qtdA) + (parseFloat(cadastroI.qtdC) * parseFloat(cadastroP.qtdChild))) * parseFloat(cadastroI.valor);
+
+            tdValorTotal.innerText = "R$ " + total;
+            trPessoa.appendChild(tdValorTotal);
 
             corpoTabela1.appendChild(trPessoa);
         }
@@ -88,16 +91,16 @@ function y() {
 
     for (var index = 0; index < novoP.length; index++) {
         var trDependentes = document.createElement("tr"); //tr é linha.
-        var td2 = document.createElement("td"); //td é célula dentro da linha.
-        var td3 = document.createElement("td"); //td é célula dentro da linha.
+        var tdNome2 = document.createElement("td"); //td é célula dentro da linha.
+        var tdNum = document.createElement("td"); //td é célula dentro da linha.
 
         var cadastroP = novoP[index];
 
-        td2.innerText=cadastroP.nome;
-        trDependentes.appendChild(td2);
+        tdNome2.innerText=cadastroP.nome;
+        trDependentes.appendChild(tdNome2);
 
-        td3.innerText=cadastroP.qtdChild;
-        trDependentes.appendChild(td3);
+        tdNum.innerText=cadastroP.qtdChild;
+        trDependentes.appendChild(tdNum);
 
         corpoTabela2.appendChild(trDependentes);
     }
@@ -108,19 +111,23 @@ function z() {
     corpoTabela3.innerHTML=""; //O corpo tabela 3 irá receber um texto.
 
     for (var index = 0; index < novoI.length; index++) {
-        var trIngredientes = document.createElement("tr"); //tr é linha.
-        var td4 = document.createElement("td"); //td é célula dentro da linha.
-        var td5 = document.createElement("td"); //td é célula dentro da linha.
+        // for (var index = 0; index < novoI.length; index++) {
+            var trIngredientes = document.createElement("tr"); //tr é linha.
+            var tdNomeIng = document.createElement("td"); //td é célula dentro da linha.
+            var tdQuantidade = document.createElement("td"); //td é célula dentro da linha.
 
-        var cadastroI = novoI[index];
+            var cadastroI = novoI[index];
+            var cadastroP = novoP[index];
+            
+            tdNomeIng.innerText = cadastroI.ingt;
+            trIngredientes.appendChild(tdNomeIng);
 
-        td4.innerText = cadastroI.ingt;
-        trIngredientes.appendChild(td4);
-
-        tqtd = parseFloat(cadastroI.qtdA) + parseFloat(cadastroI.qtdC);
-        td5.innerText = tqtd;
-        trIngredientes.appendChild(td5);
-
-        corpoTabela3.appendChild(trIngredientes);
+            contaFinal = parseFloat(cadastroI.qtdA) + (parseFloat(cadastroI.qtdC) * parseFloat(cadastroP.qtdChild));
+            
+            tdQuantidade.innerText = contaFinal;
+            trIngredientes.appendChild(tdQuantidade);
+    
+            corpoTabela3.appendChild(trIngredientes);
+        // }
     }
 }
